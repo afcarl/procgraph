27b6fd87bd34c2ebc42c522ba61f6982cd578ade
Andrea Censi <andrea@cds.caltech.edu>
2010-07-21 22:41:20 -0700

    unit tests

---
+++ src/procgraph/testing/test_values.py
@@ -0,0 +1,35 @@
+from procgraph.testing.utils import PGTestCase
+from procgraph.core.parsing import parse_value
+
+examples = { \
+            '[]': [],
+            '[1]': [1],
+            '[1,2]': [1,2],
+            '[1,[2]]': [1,[2]],
+            '[[]]': [[]],
+            '[{}]': [{}],
+            '{}': {},
+            '[{a:0}]': [{'a':0}],
+            # XXX 
+            #'{a:[]}': {"a":[]},
+            '{a:0}': {'a':0},
+            '{a:0,b:1}': {'a':0,'b':1},
+            '{a:b}': {'a':'b'},
+            '{a:{b:c}}': {'a':{'b':'c'}},
+}
+
+
+
+class SyntaxTest2(PGTestCase):
+                        
+    def testExamples(self):
+        for example, expected in examples.items():
+            print "Trying '%s'" % example
+            found = parse_value(example)
+            
+            ok = (found == expected) or (str(found)==str(expected))
+            
+            if not ok:
+                raise Exception('From "%s" got %s instead of %s.' % \
+                                (example, found, expected))
+            
