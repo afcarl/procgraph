ebcee52c624ac9c7963daa566a662e2b6d4c05a3
Andrea Censi <andrea@cds.caltech.edu>
2010-07-21 18:58:22 -0700

    Added option for showing debug

--- src/procgraph/scripts/pg.py
+++ src/procgraph/scripts/pg.py
@@ -5,15 +5,27 @@ import sys
 from procgraph.components.rawseeds import *
 from procgraph.components.cv import *
 from procgraph.core.model_loader import model_from_string
+from optparse import OptionParser
 
 
 def main():
-    file = sys.argv[1]
+     
+    parser = OptionParser()
+     
+    parser.add_option("--debug", action="store_true",
+                      default=False, dest="debug",
+                      help="Displays debug information on the model.")
+    
+    
+    (options, args) = parser.parse_args()
+    
+    
+    file = args.pop(0)
 
     model_spec = open(file).read()
     
     config = {}
-    for arg in sys.argv[2:]:
+    for arg in args:
         if '=' in arg:
             key, value = arg.split('=')
             try:
@@ -30,6 +42,10 @@ def main():
     print 'Configuration: %s' % config
     print """%s""" % model_spec
     model = model_from_string(model_spec, config=config)
+    
+    if options.debug:
+        model.summary()
+        sys.exit(0) 
     
     model.reset_execution()
     while model.has_more():
