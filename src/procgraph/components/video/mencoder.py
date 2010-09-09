5677de6b8e52ac2bfd1509d28c4d4ad7f6f54cf0
Andrea Censi <andrea@cds.caltech.edu>
2010-09-08 17:37:40 -0700

    writing out timestamps for avi files

--- src/procgraph/components/video/mencoder.py
+++ src/procgraph/components/video/mencoder.py
@@ -5,7 +5,6 @@ from procgraph import Block
 
 from procgraph.components  import check_rgb_or_grayscale
 from procgraph.core.model_loadsave import make_sure_dir_exists
-
  
  
 class MEncoder(Block):
@@ -20,8 +19,10 @@ class MEncoder(Block):
     Block.config('file', 'Output file (AVI format.)')
     Block.config('fps', 'Framerate of resulting movie.', default=10)
     Block.config('vcodec', 'Codec to use.', default='mpeg4')
-    Block.config('vbitrate', 'Bitrate -- default is reasonable.', default=1000000)
+    Block.config('vbitrate', 'Bitrate -- default is reasonable.', default=2000000)
     Block.config('quiet', "If True, suppress mencoder's messages", default=True)
+    Block.config('timestamps', "If True, also writes <file>.timestamps that"
+        " includes a line with the timestamp for each frame", default=True)
     
     def init(self): 
         self.process = None
@@ -58,8 +59,13 @@ class MEncoder(Block):
             else:
                 self.process = subprocess.Popen(args=args, stdin=subprocess.PIPE)
 
+            if self.config.timestamps:
+                self.timestamps_file = open(self.config.file + '.timestamps', 'w') 
 
             
         self.process.stdin.write(image.data)
         self.process.stdin.flush()
 
+        if self.config.timestamps:
+            self.timestamps_file.write('%s\n' % self.get_input_timestamp(0))
+            self.timestamps_file.flush()
