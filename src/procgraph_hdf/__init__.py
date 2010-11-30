001b4929a46e5251b67207a00afec3db45e034dc
Andrea Censi <andrea@cds.caltech.edu>
2010-11-30 13:53:48 -0800

    reading HDF logs

--- src/procgraph_hdf/__init__.py
+++ src/procgraph_hdf/__init__.py
@@ -6,3 +6,12 @@ You need the ``pytables`` package to be installed.
 
 import hdfwrite
 import hdfread
+
+
+# FIXME, make this easier
+import os
+from procgraph.core.registrar import default_library
+from procgraph.core.model_loader import pg_look_for_models
+dir = os.path.join(os.path.dirname(__file__), 'models')
+pg_look_for_models(default_library, additional_paths=[dir], ignore_env=True,
+                   assign_to_module='procgraph_hdf')
