b32f62e4df11e901fa5941089c960271f63bf554
Andrea Censi <andrea@cds.caltech.edu>
2010-12-03 23:32:23 -0800

    new pickle_group

--- src/procgraph/components/misc/pickling.py
+++ src/procgraph/components/misc/pickling.py
@@ -1,6 +1,7 @@
-import pickle, os
+import cPickle as pickle, os
 
 from procgraph import Block 
+from procgraph.block_utils import make_sure_dir_exists
 
 
 class Pickle(Block):
@@ -9,9 +10,39 @@ class Pickle(Block):
     Block.config('file', 'File to write to.')
     Block.input('x', 'Anything pickable.') 
         
-    def update(self):
-        dir = os.path.dirname(self.config.file)
-        if not os.path.exists(dir):
-            os.makedirs(dir)
-        pickle.dump(self.input.x, open(self.config.file, 'wb'))
+    def write(self, x, filename):
+        make_sure_dir_exists(filename)
+        with open(filename, 'wb') as f:
+            pickle.dump(x, f, protocol=pickle.HIGHEST_PROTOCOL)
  
+    def update(self):
+        self.write(self.input.x, self.config.file + '.part')
+        
+    def finish(self):
+        self.write(self.input.x, self.config.file)
+
+
+class PickleGroup(Block):
+    ''' Dumps the input as a :py:mod:`pickle` file, in the form
+        of a dictionary  signal name -> value.    
+    '''
+    Block.alias('pickle_group')
+    Block.config('file', 'File to write to.')
+    Block.input_is_variable() 
+        
+    def write(self, x, filename):
+        make_sure_dir_exists(filename)
+        with open(filename, 'wb') as f:
+            pickle.dump(x, f, protocol=pickle.HIGHEST_PROTOCOL)
+ 
+    def get_dict(self):
+        data = {}
+        for signal in self.get_input_signals_names():
+            data[signal] = self.get_input(signal)
+        return data
+    
+    def update(self):
+        self.write(self.get_dict(), self.config.file + '.part')
+        
+    def finish(self):
+        self.write(self.get_dict(), self.config.file)
