6b64b14b00e5cb0f03d634ee101319e02ab8582d
Andrea Censi <andrea@cds.caltech.edu>
2010-12-05 13:49:16 -0800

    guessing fps

--- src/procgraph_mplayer/mencoder.py
+++ src/procgraph_mplayer/mencoder.py
@@ -14,7 +14,11 @@ class MEncoder(Block):
     Block.input('image', 'H x W x 3  uint8 numpy array representing an RGB image.') 
     
     Block.config('file', 'Output file (AVI format.)')
-    Block.config('fps', 'Framerate of resulting movie.', default=10)
+    Block.config('fps', 'Framerate of resulting movie. If not specified, '
+                        'it will be guessed from data.', default=None)
+    Block.config('fps_safe', 'If the frame autodetect gives strange results, '
+                             'we use this safe value instead.', default=10)
+                        
     Block.config('vcodec', 'Codec to use.', default='mpeg4')
     Block.config('vbitrate', 'Bitrate -- default is reasonable.', default=2000000)
     Block.config('quiet', "If True, suppress mencoder's messages", default=True)
@@ -23,43 +27,86 @@ class MEncoder(Block):
     
     def init(self): 
         self.process = None 
+        self.buffer = []
         
     def update(self):
         check_rgb_or_grayscale(self, 0)
         
-        image = self.input.image        
-        h, w = image.shape[0:2]
-        fps = self.config.fps
-        
+        # Put image in a buffer -- we don't use it right away
+        image = self.get_input(0)
+        timestamp = self.get_input_timestamp(0)
+        self.buffer.append((timestamp, image))
+            
         if self.process is None:
-            vcodec = self.config.vcodec
-            vbitrate = self.config.vbitrate
-        
-            make_sure_dir_exists(self.config.file)
+            self.try_initialization()
+            
+        if self.process is not None:
+            # initialization was succesful
+            while self.buffer:
+                timestamp, image = self.buffer.pop(0)
+                self.write_value(timestamp, image) 
                 
-            format = {2: 'y8', 3: 'rgb24'}[len(image.shape)]
             
-            args = ['mencoder', '/dev/stdin', '-demuxer', 'rawvideo',
-                    '-rawvideo', 'w=%d:h=%d:fps=%d:format=%s' % (w, h, fps, format),
-                    '-ovc', 'lavc', '-lavcopts',
-                     'vcodec=%s:vbitrate=%d' % (vcodec, vbitrate),
-                     '-o', self.config.file]
-            self.info('command line: %s' % " ".join(args))
-                     
-            if self.config.quiet:
-                self.process = subprocess.Popen(args,
-                    stdin=subprocess.PIPE, stdout=open('/dev/null'),
-                                                stderr=open('/dev/null'),)
-            else:
-                self.process = subprocess.Popen(args=args, stdin=subprocess.PIPE)
-
-            if self.config.timestamps:
-                self.timestamps_file = open(self.config.file + '.timestamps', 'w') 
-
+    def try_initialization(self):
+        # If we don't have at least two frames, continue
+        if len(self.buffer) < 2:
+            return
+        
+        # Get height and width from first image
+        first_image = self.buffer[0][1]
+        
+        self.shape = first_image.shape
+        self.height = self.shape[0]
+        self.width = self.shape[1]
+        self.ndim = len(self.shape)
+        if self.ndim == 2:
+            format = 'y8'
+        else:
+            format = 'rgb24' 
+        
+        # guess the fps if we are not given the config
+        if self.config.fps is None:
+            delta = self.buffer[-1][0] - self.buffer[0][0]
+            fps = (len(self.buffer) - 1) / delta
             
+            
+            # Check for very wrong results
+            if not (3 < fps < 60):
+                self.error('Detected fps is %.2f; this seems strange to me,' 
+                           ' so I will use the safe choice fps = %.2f.' % 
+                           (fps, self.config.fps_safe))
+                fps = self.config.fps_safe
+        else:
+            fps = self.config.fps
+        
+        
+        vcodec = self.config.vcodec
+        vbitrate = self.config.vbitrate
+    
+        make_sure_dir_exists(self.config.file)
+            
+        args = ['mencoder', '/dev/stdin', '-demuxer', 'rawvideo',
+                '-rawvideo', 'w=%d:h=%d:fps=%f:format=%s' % 
+                (self.width, self.height, fps, format),
+                '-ovc', 'lavc', '-lavcopts',
+                 'vcodec=%s:vbitrate=%d' % (vcodec, vbitrate),
+                 '-o', self.config.file]
+        self.info('command line: %s' % " ".join(args))
+                 
+        if self.config.quiet:
+            self.process = subprocess.Popen(args,
+                stdin=subprocess.PIPE, stdout=open('/dev/null'),
+                                            stderr=open('/dev/null'),)
+        else:
+            self.process = subprocess.Popen(args=args, stdin=subprocess.PIPE)
+
+        if self.config.timestamps:
+            self.timestamps_file = open(self.config.file + '.timestamps', 'w') 
+
+    def write_value(self, timestamp, image):            
         self.process.stdin.write(image.data)
         self.process.stdin.flush()
 
         if self.config.timestamps:
-            self.timestamps_file.write('%s\n' % self.get_input_timestamp(0))
+            self.timestamps_file.write('%s\n' % timestamp)
             self.timestamps_file.flush()
