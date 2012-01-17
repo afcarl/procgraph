6d47b76adeb86ea4d8545a74b1733a9be148ea46
Andrea Censi <andrea@cds.caltech.edu>
2012-01-17 00:06:03 -0800

    minor

--- src/procgraph_pil/text.py
+++ src/procgraph_pil/text.py
@@ -9,10 +9,10 @@ import numpy
 
 def info(s):
     info_main('procgraph_pil/text: %s' % s)
+
+
 def error(s):
     error_main('procgraph_pil/text: %s' % s)
-    
-
 
 
 class Text(Block):
@@ -71,19 +71,19 @@ class Text(Block):
       absolute timestamp
     
     '''
-    
+
     Block.alias('text')
-    
+
     Block.config('texts', 'Text specification (see block description).')
-    
+
     Block.input('rgb', 'Input image.')
     Block.output('rgb', 'Output image with overlaid text.')
-    
-    def init(self): 
-        self.state.first_timestamp = None 
-    
+
+    def init(self):
+        self.state.first_timestamp = None
+
     def update(self):
-        
+
         # TODO: add check
         if self.state.first_timestamp is None:
             self.state.first_timestamp = self.get_input_timestamp(0)
@@ -99,16 +99,16 @@ class Text(Block):
             macros['time'] = \
                 self.get_input_timestamp(0) - self.state.first_timestamp
         macros['frame'] = self.state.frame
-        
+
         rgb = self.input.rgb
         im = Image_from_array(rgb)
         draw = ImageDraw.Draw(im)
-        
+
         # {string: "raw image", position: [10,30], halign: left, 
         # color: black, bg: white  }
         if not isinstance(self.config.texts, list):
             raise BadConfig('Expected list', self, 'texts')
-        
+
         for text in self.config.texts:
             text = text.copy()
             if not 'string' in text:
@@ -120,47 +120,51 @@ class Text(Block):
                 p[0] = rgb.shape[1] / 2
             if p[1] == 'middle':
                 p[1] = rgb.shape[0] / 2
-            if p[0] < 0: 
+            if p[0] < 0:
                 p[0] = rgb.shape[1] + p[0]
-            if p[1] < 0: 
+            if p[1] < 0:
                 p[1] = rgb.shape[0] + p[1]
-                
+
             process_text(draw, text)
-        
+
         out = im.convert("RGB")
         pixel_data = numpy.asarray(out)
-        
+
         self.output.rgb = pixel_data
-    
-    @staticmethod   
+
+    @staticmethod
     def replace(s, macros):
         ''' Expand macros in the text. '''
         return s.format(**macros)
 
-# cache of fonts
 
+# cache of fonts
 def find_file(font_name):
     try:
         #pattern = '*%s*.ttf' % font_name
         pattern = '%s.ttf' % font_name
-        a = subprocess.Popen(['locate', pattern], stdout=subprocess.PIPE); 
-        lines = a.stdout.read();
-        if len(lines) == 0:
+        a = subprocess.Popen(['locate', pattern], stdout=subprocess.PIPE)
+        lines = a.stdout.read()
+        options = lines.split('\n')
+        options = [f for f in options if os.path.exists(f)]
+        if len(options) == 0:
             error('Cannot find a file matching the pattern %r.' % pattern)
             return None
-        options = lines.split('\n')
         guess = options[0]
-        info('Found %d matches for %s, using  "%s".' % 
+        info('Found %d matches for %s, using  "%s".' %
              (len(options), pattern, guess))
         return guess
     except Exception as e:
         error('Cannot run "locate": %s' % e)
-        return None 
-    
+        return None
+
+
 fonts = {}
+
+
 def get_font(name, size):
     key = (name, size)
-    if not fonts.has_key(key):
+    if not key in fonts:
         filename = name + '.ttf'
         if not os.path.exists(filename):
             info('Could not find file %r, trying "locate"...' % filename)
@@ -173,7 +177,7 @@ def get_font(name, size):
         else:
             info('Using font in file %s' % filename)
             fonts[key] = ImageFont.truetype(filename, size)
-    
+
     return fonts[key]
 
 
@@ -185,15 +189,13 @@ def process_text(draw, t):
     size = t.get('size', 15)
     fontname = t.get('font', 'Arial')
     font = get_font(fontname, size)
-    
+
     tw, th = font.getsize(string)
     x, y = position[0], position[1]
-     
-    
+
     halign = t.get('halign', 'left')
     valign = t.get('valign', 'top')
-    
-    
+
     if halign == 'left':
         pass
     elif halign == 'right':
@@ -216,7 +218,7 @@ def process_text(draw, t):
         for a in [ [-1, 0], [1, 0], [0, 1], [0, -1], [-1, -1],
                    [-1, +1], [1, 1], [1, -1]]:
             draw.text([x + a[0], y + a[1]], string, fill=bg, font=font)
-    
+
     draw.text([x, y], string, fill=color, font=font)
-    
-    
+
+
