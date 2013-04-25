96ad40db75078141a9baf59eb9ccb27395e86543
Andrea Censi <andrea@cds.caltech.edu>
2013-04-25 09:24:03 -0700

    bumped version number

--- setup.py
+++ setup.py
@@ -67,7 +67,7 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
-version = 1.5
+version = '1.6dev1'
 
 setup(name='procgraph',
       author="Andrea Censi",
