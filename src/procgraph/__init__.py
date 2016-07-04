ef32c56ab27df3da02fc3089fecfeab5fe0e8e21
Andrea Censi <censi@mit.edu>
2016-07-04 18:41:30 -0400

    version bump

--- src/procgraph/__init__.py
+++ src/procgraph/__init__.py
@@ -1,7 +1,7 @@
 ''' ProcGraph: what you would get if Simulink was written in Python 
     and was actually useful for dealing with log data. '''
 
-__version__ = '1.10.0'
+__version__ = '1.10.1'
 
 import logging
 logging.basicConfig()
